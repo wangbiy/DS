6fc46a1b9636468e1eb95fc6706c7b587a8275fc
王碧瑶 <2487036948@qq.com>
2019-07-16 12:52:50 +0800

    栈和队列面试题

--- test_2019_7_13_2/test_2019_7_13_2/test.cpp
+++ test_2019_7_13_2/test_2019_7_13_2/test.cpp
@@ -80,6 +80,7 @@ void StackDestroy(Stack *s)
 		s->top = 0;
 	}
 }
+//C语言实现
 bool isValid(char* s){
 	int len = 0;
 	Stack st;
@@ -119,6 +120,220 @@ bool isValid(char* s){
 	StackDestroy(&st);
 	return ret;
 }
+//c++实现括号匹配问题
+//class solution{
+//public:
+//	bool isValid(string s){
+//		std::stack<char> str;
+//		int size = s.size();
+//		for (int i = 0; i < size; i++)
+//		{
+//			char ch = s[i];
+//			switch (ch)
+//			{
+//			case'(':
+//			case'[':
+//			case'{':
+//				str.push(ch);
+//				break;
+//			case')':
+//			case']':
+//			case'}':
+//			{
+//					   if (str.empty())
+//						   return false;
+//					   else
+//					   {
+//						   char left = str.top();
+//						   str.pop();
+//						   if (!((left == '('&&ch == ')') || (left == '['&&ch == ']') || (left == '{'&& ch == '}')))
+//						   {
+//							   return false;
+//						   }
+//					   }
+//					   break;
+//			}
+//			}
+//		}
+//		if (!str.empty())
+//			return false;
+//		return true;
+//	}
+//};
+//用队列实现栈
+#include <queue>
+class MyStack {
+	std::queue<int> q;
+public:
+	MyStack() {
+	}
+	void push(int x) {
+		q.push(x);
+
+	}
+	int pop() {
+		int n = q.size();
+		for (int i = 0; i<n - 1; i++)
+		{
+			int v = q.front();
+			q.pop();
+			q.push(v);
+		}
+		int v = q.front();
+		q.pop();
+		return v;
+	}
+	int top() {
+		int n = q.size();
+		for (int i = 0; i<n - 1; i++)
+		{
+			int v = q.front();
+			q.pop();
+			q.push(v);
+		}
+		int v = q.front();
+		q.pop();
+		q.push(v);
+		return v;
+	}
+	bool empty() {
+		return q.empty();
+	}
+};
+//用栈实现队列
+#include <stack>
+class MyQueue {
+	std::stack<int> in;
+	std::stack<int> out;
+public:
+	MyQueue() {
+
+	}
+	void push(int x) {
+		in.push(x);
+
+	}
+	int pop() {
+		if (out.empty())
+		{
+			int n = in.size();
+			for (int i = 0; i<n; i++)
+			{
+				int v = in.top();
+				in.pop();
+				out.push(v);
+			}
+		}
+		int v = out.top();
+		out.pop();
+		return v;
+	}
+	int peek() {
+		if (out.empty())
+		{
+			int n = in.size();
+			for (int i = 0; i<n; i++)
+			{
+				int v = in.top();
+				in.pop();
+				out.push(v);
+			}
+		}
+		int v = out.top();
+		return v;
+	}
+	bool empty() {
+
+		return in.empty() && out.empty();
+	}
+};
+//实现最小栈
+class MinStack {
+	std::stack<int> nomal;
+	std::stack<int> min;
+public:
+	MinStack() {
+
+	}
+	void push(int x) {
+		nomal.push(x);
+		if (min.empty() || x <= min.top())
+		{
+			min.push(x);
+		}
+		else
+		{
+			min.push(min.top());
+		}
+	}
+	void pop() {
+		nomal.pop();
+		min.pop();
+	}
+	int top() {
+		return nomal.top();
+	}
+	int getMin() {
+		return min.top();
+	}
+};
+//循环队列
+class MyCircularQueue{
+	int* arr;
+	int front;
+	int rear;
+	int size;
+	int capacity;
+public:
+	MyCircularQueue(int k)//初始化
+	{
+		arr = (int*)malloc(sizeof(int)*k);
+		capacity = k;
+		size = 0;
+		front = 0;
+		rear = 0;
+	}
+	bool enQueue(int value)
+	{
+		if (size >= capacity)
+		{
+			return false;
+		}
+		arr[rear] = value;
+		rear = (rear + 1) % capacity;
+		size++;
+		return true;
+	}
+	bool deQueue()
+	{
+		if (size == 0)
+			return false;
+		front = (front + 1) % capacity;
+		size--;
+		return true;
+	}
+	int Queuefront()
+	{
+		if (size == 0)
+			return -1;
+		return arr[front];
+	}
+	int Rear()
+	{
+		if (size == 0)
+			return -1;
+		int index = (rear + capacity - 1) % capacity;
+		return arr[index];
+	}
+	bool isEmpty()
+	{
+		return size == 0;
+	}
+	bool isFull()
+	{
+		return size == capacity;
+	}
+};
 int main()
 {
 	char s[] = "{[()]}";
